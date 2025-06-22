2906d666a3e9bc1999ceb3598cdae9a2d3c610ce
JimyHernandez1 <jimyhernandez35@gmail.com>
2025-06-22 13:45:43 -0400

    fetch_stats.py Version: 1

--- data-fetcher/fetch_stats.py
+++ data-fetcher/fetch_stats.py
@@ -2,24 +2,20 @@ import sqlite3
 import pandas
 from nba_api.stats.endpoints import leaguedashplayerstats
 
-def get_league_stats(season='2023-24'):
-    stats = leaguedashplayerstats.LeagueDashPlayerStats(season)
-    return stats.get_data_frames()[0]
-
-def create_views(df):
-    player_table = df[[
+def create_views(full_player_stats_df):
+    player_table = full_player_stats_df[[
         "PLAYER_ID",
-        "PLAYER_NAME",
         "TEAM_ID",
+        "PLAYER_NAME",
         "AGE"
     ]]
 
-    teams_table = df[[
+    teams_table = full_player_stats_df[[
         "TEAM_ID",
         "TEAM_ABBREVIATION"
     ]]
 
-    player_stats_table = df[[
+    player_stats_table = full_player_stats_df[[
         "PLAYER_ID",
         "GP",
         "W",
@@ -48,40 +44,40 @@ def create_views(df):
         "PTS"
     ]]
 
-    return player_table, player_stats_table, teams_table
+    return player_table, teams_table, player_stats_table
 
-def save_to_sql(teams_df, player_df, stats_df, db_path = "./database/nba_stats.db"):
-    conn = sqlite3.connect(db_path)
 
-    stats_df.to_sql(
-        name = "player_stats",
-        con = conn,
-        if_exists = "replace",
-        index = False
+def save_to_sql(player_table, teams_table, player_stats_table):
+    connection = sqlite3.connect("./database/nba_stats.db")
+
+    player_stats_table.to_sql(
+    name = "player_stats",
+    con = connection,
+    if_exists = "replace",
+    index = False
     )
 
-    player_df.to_sql(
+    player_table.to_sql(
         name = "player",
-        con = conn,
+        con = connection,
         if_exists = "replace",
         index = False
     )
 
-    teams_df.to_sql(
+    teams_table.to_sql(
         name = "teams",
-        con = conn,
+        con = connection,
         if_exists = "replace",
         index = False
     )
-
-    conn.commit()
-    conn.close()
+    connection.commit()
+    connection.close()
 
 def main():
-    full_df = get_league_stats()
-    player_df, team_df, stats_df = create_views(full_df)
-    save_to_sql(player_df, team_df, stats_df)
-
+    stats = leaguedashplayerstats.LeagueDashPlayerStats(season = '2023-24')
+    full_df = stats.get_data_frames()[0]
+    player_table, teams_table, player_stats_table = create_views(full_df)
+    save_to_sql(player_table, teams_table, player_stats_table)
 
 if __name__ == "__main__":
     main()
