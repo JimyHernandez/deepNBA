db051bd00678eb3890946825da3ac014b11f2372
JimyHernandez1 <jimyhernandez35@gmail.com>
2025-07-10 20:37:00 -0400

    Upgrades

--- server/app.js
+++ server/app.js
@@ -4,10 +4,11 @@ let app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('client/public'));
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.sendFile('index.html', {root: './client/views'});
 })
 
 module.exports = app;
-
